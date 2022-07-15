9893eba193c5f7c3fbc7b692bb4ce02ac6f4af09
bhupendrasinghacme <bhupendrasingh@acmeintech.in>
2022-07-15 17:34:51 +0530

    new update 15/7/2022

--- src/app/splash/splash.page.ts
+++ src/app/splash/splash.page.ts
@@ -9,30 +9,31 @@ import { Animation, AnimationController } from '@ionic/angular';
 export class SplashPage implements OnInit, AfterViewInit {
 
   constructor(
-    private animationCtrl: AnimationController,
+    // private animationCtrl: AnimationController,
     public router: Router
   ) {
   }
 
   ngOnInit() {
+
   }
   ngAfterViewInit() {
-    let squareC = this.animationCtrl.create()
-      .addElement(document.querySelector('.img_wrapper'))
-      .duration(1500)
-      .fromTo('transform', 'scale(0.0)', 'scale(0.8)')
-      .fromTo('opacity', '0.2', '2');
-    squareC.play();
+    // let squareC = this.animationCtrl.create()
+    //   .addElement(document.querySelector('.img_wrapper'))
+    //   .duration(1500)
+    //   .fromTo('transform', 'scale(0.0)', 'scale(0.8)')
+    //   .fromTo('opacity', '0.2', '2');
+    // squareC.play();
 
-    setTimeout(() => {
-      let squareA = this.animationCtrl.create()
-        .addElement(document.querySelector('.animate_text'))
-        .duration(1500)
-        .fromTo('transform', 'scale(0.0)', 'scale(1.5)')
-        .fromTo('opacity', '0.2', '2');
+    // setTimeout(() => {
+    //   let squareA = this.animationCtrl.create()
+    //     .addElement(document.querySelector('.animate_text'))
+    //     .duration(1500)
+    //     .fromTo('transform', 'scale(0.0)', 'scale(1.5)')
+    //     .fromTo('opacity', '0.2', '2');
 
-      squareA.play();
-    }, 1000)
+    //   squareA.play();
+    // }, 1000)
     setTimeout(() => {
       this.router.navigate(['/home']);
     }, 5000)
